d8f48ffd44114e5bd576a8275d3dbf2b6e498295
samisasm1 <forsamisams@gmail.com>
2024-03-19 21:34:08 +0300

    average of three numbers

---
+++ averageofthree.py
@@ -0,0 +1,8 @@
+num1  = float(input("Please Enter first number"))
+num2 = float(input("please Enter second number"))
+num3 = float(input("please Enter third number"))
+
+sum = num1+num2+ num3
+avarage = sum/3
+print(avarage)
+
